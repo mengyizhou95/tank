14e298679cac5c7c8ca7818c2883d162421b7dd1
meng <mengyizhou95@gmail.com>
2020-11-20 23:17:24 -0500

    day1

---
+++ src/com/mashibing/tank/Main.java
@@ -0,0 +1,7 @@
+package com.mashibing.tank;
+
+public class Main {
+    public static void main(String[] args) {
+        TankFrame tf = new TankFrame();
+    }
+}
---
+++ src/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,85 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame{
+
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+//        x += 10;
+//        y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+
+        boolean bl = false;
+        boolean bu = false;
+        boolean br = false;
+        boolean bd = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
